b408223409e80b45400c70e44f88539cbb269e00
Naufal-farhan <nefener123@gmail.com>
2026-09-23 20:49:00 +0700

    js 7.1

--- Jobsheet/Jobsheet-07/api/includes/footer.php
+++ Jobsheet/Jobsheet-07/api/includes/footer.php
@@ -2,7 +2,7 @@
         <p>&copy; 2026 ADMIN RENTAL &mdash; Jobsheet 7</p>
     </footer>
     <!-- Jalur JS langsung mengarah ke assets/js/app.js -->
-    <script src="assets/js/app.js"></script>
+    <script src="../public/assets/js/app.js"></script>
 </body>
 
 </html>
